685f4709f4d1ba884672eca47de201d3a2c536c3
Maxim <maksim.pantelidi@yandex.ru>
2023-06-25 19:43:09 +0300

    header and footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("Hello")
+const dropDowns = document.querySelectorAll('.dropdown');
+const selected = document.querySelector('.selected');
+const select = document.querySelector('.select');
+const caret = document.querySelector('.caret');
+const menu = document.querySelector('.dropdown__menu');
+const options = document.querySelectorAll('.dropdown__menu-item');
+
+dropDowns.forEach(() => {
+    select.addEventListener('click', () => {
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('dropdown__menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('dropdown__menu-open');
+        });
+    });
+});
